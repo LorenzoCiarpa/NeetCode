1c5713ce7d06175df6585d9c160057f414bcf46c
Lorenzo Ciarpaglini <77929834+LorenzoCiarpa@users.noreply.github.com>
2026-08-31 17:55:05 +0200

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        lo, high = 1, max(piles)
+
+        while lo <= high:
+            mid = (lo + high) // 2
+
+            res = feasible(mid, piles, h)
+
+            if res == 1: 
+                high = mid -1
+            else:
+                lo = mid + 1
+        
+        if res == 1:
+            return mid
+        else:
+            return mid + 1
+
+
+
+def feasible(num, piles, h):
+    counter = 0
+    for pile in piles:
+        counter += math.ceil(pile/num)
+    
+    if counter <= h:
+        return 1
+    else: 
+        return -1
