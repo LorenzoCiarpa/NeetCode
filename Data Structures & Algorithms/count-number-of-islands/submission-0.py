cee1d69540d01ba2ad83e59ec3464a17ca0b3130
Lorenzo Ciarpaglini <77929834+LorenzoCiarpa@users.noreply.github.com>
2026-09-03 12:12:56 +0200

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        visit = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i, j) not in visit and grid[i][j] == "1":
+                    self.aux(grid, i, j, visit)
+                    count += 1
+        return count
+    
+    def aux(self, grid, r, c, visit) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        if min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in visit:
+            return
+        
+        if grid[r][c] == "0":
+            return
+        
+        visit.add((r, c))
+        
+        self.aux(grid, r+1, c, visit)
+        self.aux(grid, r-1, c, visit)
+        self.aux(grid, r, c+1, visit)
+        self.aux(grid, r, c-1, visit)
+        
+        return
