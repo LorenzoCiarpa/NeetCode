775d556ed64dccc9e7e0437d0255101c969ef42f
Lorenzo Ciarpaglini <77929834+LorenzoCiarpa@users.noreply.github.com>
2026-09-05 14:52:14 +0200

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {}
+
+        for i in range(numCourses):
+            adj[i] = []
+        
+        for a, b in prerequisites:
+            adj[a].append(b)
+        
+        visit = set()
+        path = set()
+        topSort = []
+        for n in range(numCourses):
+            result = self.dfs(adj, n, visit, topSort, path)
+            if not result:
+                return []
+        return topSort
+
+
+    def dfs(self, adj, src, visit, topSort, path) -> bool:
+        if src in path:
+            return False
+        if src in visit:
+            return True
+        
+        
+        visit.add(src)
+        path.add(src)
+        for dst in adj[src]:
+            result = self.dfs(adj, dst, visit, topSort, path)
+            if not result:
+                return result
+        
+        path.remove(src)
+        topSort.append(src)
+        return True
